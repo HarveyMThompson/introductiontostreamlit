c0ceff140145e27d8bf729506137de3c24027344
HarveyMThompson <39711129+HarveyMThompson@users.noreply.github.com>
2025-02-16 13:42:36 +0000

    Add files via upload

---
+++ examples/forms/forms3.py
@@ -0,0 +1,10 @@
+# forms3.py
+
+import streamlit as st
+
+form = st.form("my_form")
+form.slider("Inside the form")
+st.slider("Outside the form")
+
+# Now add a submit button to the form:
+form.form_submit_button("Submit")
