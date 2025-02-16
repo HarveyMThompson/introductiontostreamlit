c8b220ca4830abe26a58736ffe4fe201439f5d0a
HarveyMThompson <39711129+HarveyMThompson@users.noreply.github.com>
2025-02-16 13:39:05 +0000

    Add files via upload

---
+++ examples/customclasses/enumexample.py
@@ -0,0 +1,20 @@
+# enumexample.py
+"""
+Created on Thu Jun  6 10:03:48 2024
+
+@author: harveythompson
+"""
+
+from enum import Enum
+import streamlit as st
+
+# class syntax
+class Color(Enum):
+    RED = 1
+    GREEN = 2
+    BLUE = 3
+
+selected_colors = set(st.multiselect("Pick colors", options=Color))
+
+if selected_colors == {Color.RED, Color.GREEN}:
+    st.write("Hooray, you found the color YELLOW!")
